721797511fba8fc3e1906751ff8965669807ed5e
Esteban Caccin <80750747+Bigmasters@users.noreply.github.com>
2021-05-18 05:39:29 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* PEQUEÑO SCRIPT ABRE Y CIERRA EL NAVEGADOR EN DISPOSITIVOS MOBILES */
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+
+function toggleMenu() {
+  menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
